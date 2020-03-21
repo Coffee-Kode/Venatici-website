71bf510faad0833f755e106813275c616d9ddccd
NintendoD <rodrimanriquez10@gmail.com>
2020-03-21 20:40:54 -0300

    added class area for center section content and margin y padding deleted

--- application/views/website/home.php
+++ application/views/website/home.php
@@ -112,56 +112,20 @@
         <hr>
 
         <!-- Start Section: Programs and calls-->
-        <div id="section-programs-calls" class="area">
+        <section id="section-programs-calls" class="area">
 
             <h2 id="title_services" class="h2 display-4 text-center">Programas y Convocatorias</h2>
-            <!--Start Section: Carousel Events-->
-            <!--Carousel Wrapper-->
-            <div id="carousel" class="carousel slide carousel-fade" data-ride="carousel">
-                <!--Indicators-->
-                <ol class="carousel-indicators">
-                    <li data-target="#carousel" data-slide-to="0" class="active"></li>
-                    <li data-target="#carousel" data-slide-to="1"></li>
-                    <li data-target="#carousel" data-slide-to="2"></li>
-                </ol>
-                <!--/.Indicators-->
-                <!--Slides-->
-                <div class="carousel-inner" role="listbox">
-                    <!--First slide-->
-                    <div class="carousel-item active">
-                        <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(130).jpg"
-                            alt="First slide">
-                    </div>
-                    <!--/First slide-->
-                    <!--Second slide-->
-                    <div class="carousel-item">
-                        <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(129).jpg"
-                            alt="Second slide">
-                    </div>
-                    <!--/Second slide-->
-                    <!--Third slide-->
-                    <div class="carousel-item">
-                        <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg"
-                            alt="Third slide">
-                    </div>
-                    <!--/Third slide-->
-                </div>
-                <!--/.Slides-->
-                <!--Controls-->
-                <a class="carousel-control-prev" href="#carousel-example-1z" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carousel-example-1z" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
-                <!--/.Controls-->
-            </div>
-            <!--/.Carousel Wrapper-->
-            <!--Finish Section: Carousel Events-->
 
-        </div>
+            <div class="container">
+                <div class="row">
+                    <div>
+                        
+                    </div>
+                </div>
+
+            </div>
+
+        </section>
         <!-- Finish Section: Programs and calls-->
 
         <hr>
